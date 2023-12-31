4a6e201b968ad131daccf8d378d233d0eb9bf61a
subas-roy <subasroy46@gmail.com>
2023-12-31 16:58:41 +0600

    added blog and newsletter

---
+++ src/Components/Pages/Home/Blog/Blog.jsx
@@ -0,0 +1,57 @@
+
+const Blog = () => {
+  return (
+    <>
+    <div class="container mx-auto mt-20">
+      <h2 className=" text-center	text-4xl">Latest Blogs</h2>
+      <p className="text-center	pb-6">Lorem ipsum dolor sit amet</p>
+      <div className="flex">
+        <div className="card w-100 bg-base-100 shadow-xl mr-8">
+          <figure><img src="https://daisyui.com/images/stock/photo-1606107557195-0e29a4b5b4aa.jpg" alt="Shoes" /></figure>
+          <div className="card-body">
+            <h2 className="card-title">
+              Shoes!
+              <div className="badge badge-secondary">NEW</div>
+            </h2>
+            <p>If a dog chews shoes whose shoes does he choose?</p>
+            <div className="card-actions justify-end">
+              <div className="badge badge-outline">Fashion</div> 
+              <div className="badge badge-outline">Products</div>
+            </div>
+          </div>
+        </div>
+        <div className="card w-100 bg-base-100 shadow-xl">
+          <figure><img src="https://daisyui.com/images/stock/photo-1606107557195-0e29a4b5b4aa.jpg" alt="Shoes" /></figure>
+          <div className="card-body">
+            <h2 className="card-title">
+              Shoes!
+              <div className="badge badge-secondary">NEW</div>
+            </h2>
+            <p>If a dog chews shoes whose shoes does he choose?</p>
+            <div className="card-actions justify-end">
+              <div className="badge badge-outline">Fashion</div> 
+              <div className="badge badge-outline">Products</div>
+            </div>
+          </div>
+        </div>
+        <div className="card w-100 bg-base-100 shadow-xl ml-8">
+          <figure><img src="https://daisyui.com/images/stock/photo-1606107557195-0e29a4b5b4aa.jpg" alt="Shoes" /></figure>
+          <div className="card-body">
+            <h2 className="card-title">
+              Shoes!
+              <div className="badge badge-secondary">NEW</div>
+            </h2>
+            <p>If a dog chews shoes whose shoes does he choose?</p>
+            <div className="card-actions justify-end">
+              <div className="badge badge-outline">Fashion</div> 
+              <div className="badge badge-outline">Products</div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+    </>
+  );
+};
+
+export default Blog;
