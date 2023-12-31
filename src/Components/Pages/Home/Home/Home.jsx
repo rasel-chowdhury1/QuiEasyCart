a782acce6d73629571951a2d1883801063a78963
Nazrul islam <nazrulislamsuton06@gmail.com>
2023-12-31 21:34:34 +0600

    added best offers

--- src/Components/Pages/Home/Home/Home.jsx
+++ src/Components/Pages/Home/Home/Home.jsx
@@ -2,13 +2,17 @@ import React from 'react';
 import Slider from '../Slider/Slider';
 import Brand from '../Brand/Brand';
 import TopRated from '../SpecialProducts/TopRated/TopRated';
+import BestSale from '../SpecialProducts/BestSale/BestSale';
+import SpecialProduct from '../SpecialProducts/SpecialProduct';
+import BestOffer from '../BestOffer/BestOffer';
 
 const Home = () => {
     return (
         <div>
             <Slider></Slider>
             <Brand></Brand>
-            <TopRated></TopRated>
+            <BestOffer></BestOffer>
+            <SpecialProduct ></SpecialProduct>
         </div>
     );
 };
