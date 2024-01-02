fa9a32a7e38908b43896bdd9a0ffa8d72395e31b
hridoythebest <hridoythebest@gmail.com>
2024-01-02 10:18:12 +0600

    code updated

---
+++ src/Components/AdminDashboard/Dashboard/Products.jsx
@@ -0,0 +1,255 @@
+import React from 'react';
+
+const Products = () => {
+    return (
+        <div>
+            <div className="collapse bg-base-200">
+                <input type="checkbox" className="peer" />
+                <div className="collapse-title bg-primary text-primary-content peer-checked:bg-secondary peer-checked:text-secondary-content">
+                    Click me to show/hide content
+                </div>
+                <div className="collapse-content bg-primary text-primary-content peer-checked:bg-secondary peer-checked:text-secondary-content">
+                    <p>hello</p>
+                </div>
+            </div>
+            <div className="diff aspect-[16/9]">
+                <div className="diff-item-1">
+                    <img alt="daisy" src="https://daisyui.com/images/stock/photo-1560717789-0ac7c58ac90a.jpg" />
+                </div>
+                <div className="diff-item-2">
+                    <img alt="daisy" src="https://daisyui.com/images/stock/photo-1560717789-0ac7c58ac90a-bw.jpg" />
+                </div>
+                <div className="diff-resizer"></div>
+            </div>
+
+            <div className="flex justify-center gap-1 my-1 w-full">
+                <kbd className="kbd">q</kbd>
+                <kbd className="kbd">w</kbd>
+                <kbd className="kbd">e</kbd>
+                <kbd className="kbd">r</kbd>
+                <kbd className="kbd">t</kbd>
+                <kbd className="kbd">y</kbd>
+                <kbd className="kbd">u</kbd>
+                <kbd className="kbd">i</kbd>
+                <kbd className="kbd">o</kbd>
+                <kbd className="kbd">p</kbd>
+            </div>
+            <div className="flex justify-center gap-1 my-1 w-full">
+                <kbd className="kbd">a</kbd>
+                <kbd className="kbd">s</kbd>
+                <kbd className="kbd">d</kbd>
+                <kbd className="kbd">f</kbd>
+                <kbd className="kbd">g</kbd>
+                <kbd className="kbd">h</kbd>
+                <kbd className="kbd">j</kbd>
+                <kbd className="kbd">k</kbd>
+                <kbd className="kbd">l</kbd>
+            </div>
+            <div className="flex justify-center gap-1 my-1 w-full">
+                <kbd className="kbd">z</kbd>
+                <kbd className="kbd">x</kbd>
+                <kbd className="kbd">c</kbd>
+                <kbd className="kbd">v</kbd>
+                <kbd className="kbd">b</kbd>
+                <kbd className="kbd">n</kbd>
+                <kbd className="kbd">m</kbd>
+                <kbd className="kbd">/</kbd>
+            </div>
+
+            <div className="overflow-x-auto">
+                <table className="table">
+                    {/* head */}
+                    <thead>
+                        <tr>
+                            <th>
+                                <label>
+                                    <input type="checkbox" className="checkbox" />
+                                </label>
+                            </th>
+                            <th>Name</th>
+                            <th>Job</th>
+                            <th>Favorite Color</th>
+                            <th></th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {/* row 1 */}
+                        <tr>
+                            <th>
+                                <label>
+                                    <input type="checkbox" className="checkbox" />
+                                </label>
+                            </th>
+                            <td>
+                                <div className="flex items-center gap-3">
+                                    <div className="avatar">
+                                        <div className="mask mask-squircle w-12 h-12">
+                                            <img src="/tailwind-css-component-profile-2@56w.png" alt="Avatar Tailwind CSS Component" />
+                                        </div>
+                                    </div>
+                                    <div>
+                                        <div className="font-bold">Hart Hagerty</div>
+                                        <div className="text-sm opacity-50">United States</div>
+                                    </div>
+                                </div>
+                            </td>
+                            <td>
+                                Zemlak, Daniel and Leannon
+                                <br />
+                                <span className="badge badge-ghost badge-sm">Desktop Support Technician</span>
+                            </td>
+                            <td>Purple</td>
+                            <th>
+                                <button className="btn btn-ghost btn-xs">details</button>
+                            </th>
+                        </tr>
+                        {/* row 2 */}
+                        <tr>
+                            <th>
+                                <label>
+                                    <input type="checkbox" className="checkbox" />
+                                </label>
+                            </th>
+                            <td>
+                                <div className="flex items-center gap-3">
+                                    <div className="avatar">
+                                        <div className="mask mask-squircle w-12 h-12">
+                                            <img src="/tailwind-css-component-profile-3@56w.png" alt="Avatar Tailwind CSS Component" />
+                                        </div>
+                                    </div>
+                                    <div>
+                                        <div className="font-bold">Brice Swyre</div>
+                                        <div className="text-sm opacity-50">China</div>
+                                    </div>
+                                </div>
+                            </td>
+                            <td>
+                                Carroll Group
+                                <br />
+                                <span className="badge badge-ghost badge-sm">Tax Accountant</span>
+                            </td>
+                            <td>Red</td>
+                            <th>
+                                <button className="btn btn-ghost btn-xs">details</button>
+                            </th>
+                        </tr>
+                        {/* row 3 */}
+                        <tr>
+                            <th>
+                                <label>
+                                    <input type="checkbox" className="checkbox" />
+                                </label>
+                            </th>
+                            <td>
+                                <div className="flex items-center gap-3">
+                                    <div className="avatar">
+                                        <div className="mask mask-squircle w-12 h-12">
+                                            <img src="/tailwind-css-component-profile-4@56w.png" alt="Avatar Tailwind CSS Component" />
+                                        </div>
+                                    </div>
+                                    <div>
+                                        <div className="font-bold">Marjy Ferencz</div>
+                                        <div className="text-sm opacity-50">Russia</div>
+                                    </div>
+                                </div>
+                            </td>
+                            <td>
+                                Rowe-Schoen
+                                <br />
+                                <span className="badge badge-ghost badge-sm">Office Assistant I</span>
+                            </td>
+                            <td>Crimson</td>
+                            <th>
+                                <button className="btn btn-ghost btn-xs">details</button>
+                            </th>
+                        </tr>
+                        {/* row 4 */}
+                        <tr>
+                            <th>
+                                <label>
+                                    <input type="checkbox" className="checkbox" />
+                                </label>
+                            </th>
+                            <td>
+                                <div className="flex items-center gap-3">
+                                    <div className="avatar">
+                                        <div className="mask mask-squircle w-12 h-12">
+                                            <img src="/tailwind-css-component-profile-5@56w.png" alt="Avatar Tailwind CSS Component" />
+                                        </div>
+                                    </div>
+                                    <div>
+                                        <div className="font-bold">Yancy Tear</div>
+                                        <div className="text-sm opacity-50">Brazil</div>
+                                    </div>
+                                </div>
+                            </td>
+                            <td>
+                                Wyman-Ledner
+                                <br />
+                                <span className="badge badge-ghost badge-sm">Community Outreach Specialist</span>
+                            </td>
+                            <td>Indigo</td>
+                            <th>
+                                <button className="btn btn-ghost btn-xs">details</button>
+                            </th>
+                        </tr>
+                    </tbody>
+                    {/* foot */}
+                    <tfoot>
+                        <tr>
+                            <th></th>
+                            <th>Name</th>
+                            <th>Job</th>
+                            <th>Favorite Color</th>
+                            <th></th>
+                        </tr>
+                    </tfoot>
+
+                </table>
+            </div>
+            <ul className="timeline">
+                <li>
+                    <div className="timeline-start timeline-box">First Macintosh computer</div>
+                    <div className="timeline-middle">
+                        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" className="w-5 h-5 text-primary"><path fillRule="evenodd" d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.857-9.809a.75.75 0 00-1.214-.882l-3.483 4.79-1.88-1.88a.75.75 0 10-1.06 1.061l2.5 2.5a.75.75 0 001.137-.089l4-5.5z" clipRule="evenodd" /></svg>
+                    </div>
+                    <hr className="bg-primary" />
+                </li>
+                <li>
+                    <hr className="bg-primary" />
+                    <div className="timeline-middle">
+                        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" className="w-5 h-5 text-primary"><path fillRule="evenodd" d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.857-9.809a.75.75 0 00-1.214-.882l-3.483 4.79-1.88-1.88a.75.75 0 10-1.06 1.061l2.5 2.5a.75.75 0 001.137-.089l4-5.5z" clipRule="evenodd" /></svg>
+                    </div>
+                    <div className="timeline-end timeline-box">iMac</div>
+                    <hr className="bg-primary" />
+                </li>
+                <li>
+                    <hr className="bg-primary" />
+                    <div className="timeline-start timeline-box">iPod</div>
+                    <div className="timeline-middle">
+                        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" className="w-5 h-5 text-primary"><path fillRule="evenodd" d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.857-9.809a.75.75 0 00-1.214-.882l-3.483 4.79-1.88-1.88a.75.75 0 10-1.06 1.061l2.5 2.5a.75.75 0 001.137-.089l4-5.5z" clipRule="evenodd" /></svg>
+                    </div>
+                    <hr />
+                </li>
+                <li>
+                    <hr />
+                    <div className="timeline-middle">
+                        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" className="w-5 h-5"><path fillRule="evenodd" d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.857-9.809a.75.75 0 00-1.214-.882l-3.483 4.79-1.88-1.88a.75.75 0 10-1.06 1.061l2.5 2.5a.75.75 0 001.137-.089l4-5.5z" clipRule="evenodd" /></svg>
+                    </div>
+                    <div className="timeline-end timeline-box">iPhone</div>
+                    <hr />
+                </li>
+                <li>
+                    <hr />
+                    <div className="timeline-start timeline-box">Apple Watch</div>
+                    <div className="timeline-middle">
+                        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" className="w-5 h-5"><path fillRule="evenodd" d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.857-9.809a.75.75 0 00-1.214-.882l-3.483 4.79-1.88-1.88a.75.75 0 10-1.06 1.061l2.5 2.5a.75.75 0 001.137-.089l4-5.5z" clipRule="evenodd" /></svg>
+                    </div>
+                </li>
+            </ul>
+
+        </div>
+    );
+};
+
+export default Products;
