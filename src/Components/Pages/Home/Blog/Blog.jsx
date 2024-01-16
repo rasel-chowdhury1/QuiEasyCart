aaf82f0c86147ee3575851aad81581d4d3bfa3a3
hridoythebest <hridoythebest@gmail.com>
2024-01-16 19:45:07 +0600

    blog rendered on home page dynamically

--- src/Components/Pages/Home/Blog/Blog.jsx
+++ src/Components/Pages/Home/Blog/Blog.jsx
@@ -1,55 +1,48 @@
+import { useEffect, useState } from "react";
+import { Link } from "react-router-dom";
 
 const Blog = () => {
+  const [blogs, setBlogs] = useState([])
+
+  useEffect(() => {
+    fetch("http://localhost:3000/allBlogs")
+      .then(res => res.json())
+      .then(data => setBlogs(data))
+  }, []);
+
+
+
   return (
     <>
-    <div className="container mx-auto mt-20">
-      <h2 className=" text-center	text-4xl">Latest Blogs</h2>
-      <p className="text-center	pb-6">Lorem ipsum dolor sit amet</p>
-      <div className="flex">
-        <div className="card w-100 bg-base-100 shadow-xl mr-8">
-          <figure><img src="https://daisyui.com/images/stock/photo-1606107557195-0e29a4b5b4aa.jpg" alt="Shoes" /></figure>
+      <div className="container mx-auto mt-20">
+        <h2 className=" text-center	text-4xl">Latest Blogs</h2>
+        <p className="text-center	pb-6">Lorem ipsum dolor sit amet</p>
+        <div className="flex">
+          {
+            blogs.map(blog => 
+              < div key={blog._id} className="card w-100 bg-base-100 shadow-xl mr-8">
+          <figure><img className="w-full h-md" src={blog.image} alt="Shoes" /></figure>
           <div className="card-body">
             <h2 className="card-title">
-              Shoes!
+             {blog.title}
               <div className="badge badge-secondary">NEW</div>
             </h2>
-            <p>If a dog chews shoes whose shoes does he choose?</p>
+            <p>{blog.content}</p>
+            <Link to={`/blogdetails/${blog._id}`} className="btn btn-accent  p-2 m-2" >View Blog</Link>
             <div className="card-actions justify-end">
-              <div className="badge badge-outline">Fashion</div> 
+              <div className="badge badge-outline">Fashion</div>
               <div className="badge badge-outline">Products</div>
             </div>
           </div>
         </div>
-        <div className="card w-100 bg-base-100 shadow-xl">
-          <figure><img src="https://daisyui.com/images/stock/photo-1606107557195-0e29a4b5b4aa.jpg" alt="Shoes" /></figure>
-          <div className="card-body">
-            <h2 className="card-title">
-              Shoes!
-              <div className="badge badge-secondary">NEW</div>
-            </h2>
-            <p>If a dog chews shoes whose shoes does he choose?</p>
-            <div className="card-actions justify-end">
-              <div className="badge badge-outline">Fashion</div> 
-              <div className="badge badge-outline">Products</div>
-            </div>
-          </div>
-        </div>
-        <div className="card w-100 bg-base-100 shadow-xl ml-8">
-          <figure><img src="https://daisyui.com/images/stock/photo-1606107557195-0e29a4b5b4aa.jpg" alt="Shoes" /></figure>
-          <div className="card-body">
-            <h2 className="card-title">
-              Shoes!
-              <div className="badge badge-secondary">NEW</div>
-            </h2>
-            <p>If a dog chews shoes whose shoes does he choose?</p>
-            <div className="card-actions justify-end">
-              <div className="badge badge-outline">Fashion</div> 
-              <div className="badge badge-outline">Products</div>
-            </div>
-          </div>
-        </div>
+
+            )
+            
+          }
+
+
       </div>
-    </div>
+    </div >
     </>
   );
 };
