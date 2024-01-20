571d830cce00641f933ccd0cdc40083a60edd54f
rasel-chowdhury1 <rasel.chowdhury0001@gmail.com>
2024-01-20 23:59:05 +0600

    updated

--- src/Providers/AuthProvider.jsx
+++ src/Providers/AuthProvider.jsx
@@ -1,6 +1,7 @@
 import React, { createContext, useEffect, useState } from 'react';
 import { GoogleAuthProvider, createUserWithEmailAndPassword, getAuth, onAuthStateChanged,signInWithEmailAndPassword, signInWithPopup, signOut } from "firebase/auth";
 import app from '../Firebase/Firebase.config';
+import axios from 'axios';
 
 export const AuthContext = createContext(null);
 
@@ -37,7 +38,20 @@ const AuthProvider = ({children}) => {
     useEffect( () =>{
         const unsubscribe = onAuthStateChanged(auth, currentUser =>{
             setUser(currentUser);
-            setLoading(false);
+
+            //get and set token
+            if(currentUser){
+                axios.post('http://localhost:3000/jwt', {email: currentUser.email})
+                .then(data => {
+                    console.log(data.data.token)
+                    localStorage.setItem('access-token', data.data.token)
+                    setLoading(false);
+                })
+            }
+            else{
+                localStorage.removeItem('access-token')
+            }
+
         })
 
         return () =>{
