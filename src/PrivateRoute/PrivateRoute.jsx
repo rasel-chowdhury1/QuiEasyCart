8d1c4eaf5186bff0007eff722d31dd1ceb6ea727
rasel-chowdhury1 <rasel.chowdhury0001@gmail.com>
2024-01-21 17:22:39 +0600

    working private route and jsonwebtoken

---
+++ src/PrivateRoute/PrivateRoute.jsx
@@ -0,0 +1,20 @@
+import React, { useContext } from 'react';
+import { Navigate, useLocation } from 'react-router-dom';
+import { AuthContext } from './../Providers/AuthProvider';
+
+const PrivateRoute = ({children}) => {
+    const {user,loading} = useContext(AuthContext);
+    const location = useLocation();
+
+    if(loading){
+        return <div className='h-screen grid justify-items-center content-center'><span className="loading loading-bars loading-lg"></span><p>Loading...</p></div>
+    }
+
+    if(user){
+        return children;
+    }
+    return (<Navigate to='/login' state={{from: location}} replace></Navigate>
+    );
+};
+
+export default PrivateRoute;
