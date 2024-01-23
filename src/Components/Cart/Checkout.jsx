33d47d7ffa7f937c897ee70e6eb41b99f17a27cf
hridoythebest <hridoythebest@gmail.com>
2024-01-23 15:41:05 +0600

    checkout page user data auto fill done

--- src/Components/Cart/Checkout.jsx
+++ src/Components/Cart/Checkout.jsx
@@ -1,17 +1,22 @@
-import React, { useContext } from 'react';
+import React, { useContext, useEffect, useState } from 'react';
 import { RiDeleteBin6Line } from 'react-icons/ri';
 import { useLocation, useNavigate } from 'react-router-dom';
 import { useForm } from 'react-hook-form';
 import { AuthContext } from '../../Providers/AuthProvider';
+
+
 const Checkout = () => {
   const { user, loading } = useContext(AuthContext);
 
+  const userId = localStorage.getItem('userId')
+
+  console.log("User ID :", userId)
   const data = useLocation()
 
   const { register, handleSubmit, formState: { errors }, reset } = useForm();
   const navigate = useNavigate()
-  const userId = localStorage.getItem('userId')
   const today = new Date()
+  
   console.log(today.toLocaleDateString("en-US"))
   const onSubmit = (formData) => {
     console.log('this is form data ', formData)
@@ -42,112 +47,30 @@ const Checkout = () => {
       })
   }
 
+  const [userData, setUserData] = useState({});
+  const {firstName, lastName, phone, address} = userData;
+  console.log(firstName, lastName, phone, address)
+  // console.log("User Data : ", userData)
+
+  const getUser = () => {
+    fetch(`https://quieasycarts.onrender.com/user/${userId}`)
+        .then(res => res.json())
+        .then(result => {
+            console.log(result);
+            setUserData(result)
+        })
+}
+  
+   
+useEffect( () => {
+  getUser()
+} , [])
+
 
   return (
     <div>
-      <div className="max-w-5xl mx-auto mt-8 p-4 flex flex-wrap">
-        {/* Left Section - User Information */}
-        <div className="sm:w-full lg:w-1/2 pr-4  mb-6">
-          <div className="max-w-md mx-auto mt-8 p-4 border rounded shadow-md">
-            <h2 className="text-2xl font-bold mb-4">Checkout</h2>
-            <form onSubmit={handleSubmit(onSubmit)}>
-              {/* Shipping Information */}
-              <div className="mb-4">
-                <h3 className="text-xl font-semibold mb-2">Shipping Information</h3>
-                <div className="grid grid-cols-2 gap-4">
-                  <div>
-                    <label htmlFor="firstName" className="block text-sm font-medium text-gray-700">
-                      First Name
-                    </label>
-                    <input
-                      type="text"
-                      id="firstName"
-                      name="firstName"
-                      className="mt-1 p-2 w-full border rounded focus:outline-none focus:border-blue-500"
-                      {...register("firstName", { required: true })}
-                    />
-                  </div>
-                  <div>
-                    <label htmlFor="lastName" className="block text-sm font-medium text-gray-700">
-                      Last Name
-                    </label>
-                    <input
-                      type="text"
-                      id="lastName"
-                      name="lastName"
-                      className="mt-1 p-2 w-full border rounded focus:outline-none focus:border-blue-500"
-                      {...register("lastName", { required: true })}
-                    />
-                  </div>
-                </div>
-                <div className="mt-4">
-                  <label htmlFor="address" className="block text-sm font-medium text-gray-700">
-                    Address
-                  </label>
-                  <input
-                    type="text"
-                    id="address"
-                    name="address"
-                    className="mt-1 p-2 w-full border rounded focus:outline-none focus:border-blue-500"
-                    {...register("address", { required: true })}
-                  />
-                </div>
-                {/* Add more shipping fields as needed */}
-              </div>
+      <div className="max-w-5xl mx-auto mt-8 p-4 flex ">
 
-              {/* Payment Information */}
-              <div className="mb-4">
-                <h3 className="text-xl font-semibold mb-2">Payment Information</h3>
-                <div>
-                  <label htmlFor="cardNumber" className="block text-sm font-medium text-gray-700">
-                    Currency
-                  </label>
-                  <select {...register("currency", { required: true })}
-                    className="select select-bordered mt-1 p-2 w-full border rounded focus:outline-none focus:border-blue-500">
-                    <option value="BDT">BDT</option>
-                    <option value="USD">USD</option>
-                    <option value="RMB">RMB</option>
-                    <option value="EURO">EURO</option>
-                  </select>
-                </div>
-                <div className="mt-4">
-                  <label htmlFor="expiryDate" className="block text-sm font-medium text-gray-700">
-                    Mobile
-                  </label>
-                  <input
-                    type="text"
-                    id="mobile"
-                    name="mobile"
-                    className="mt-1 p-2 w-full border rounded focus:outline-none focus:border-blue-500"
-                    {...register("mobile", { required: true })}
-                  />
-                </div>
-                <div className="mt-4">
-                  <label htmlFor="cvv" className="block text-sm font-medium text-gray-700">
-                    Amount
-                  </label>
-                  <input
-                    type="text"
-                    id="amount"
-                    name="amount"
-                    value={data.state && parseInt(data.state.total).toFixed(2)}
-                    className="mt-1 p-2 w-full border rounded focus:outline-none focus:border-blue-500"
-                    {...register("amount", { required: true })}
-                  />
-                </div>
-              </div>
-
-              {/* Place Order Button */}
-              <div className="mt-6">
-                <input
-                  type="submit"
-                  value='Place Order'
-                  className="w-full bg-blue-500 text-white p-2 rounded focus:outline-none hover:bg-blue-700"
-                />
-              </div>
-            </form>
-          </div>
-        </div>
 
         {/* Right Section - Order Items */}
         <div className="sm:w-full lg:w-2/3 pl-4">
@@ -196,6 +119,116 @@ const Checkout = () => {
               <h4 className='py-3 text-xl font-bold'>Grand Total: ${data.state && data.state.total.toFixed(2)}</h4>
             </div>
 
+          </div>
+        </div>
+
+
+
+
+        {/* Left Section - User Information */}
+        <div className="sm:w-full lg:w-1/2 pr-4  mb-6">
+          <div className="max-w-md mx-auto mt-8 p-4 border rounded shadow-md">
+            <h2 className="text-2xl font-bold mb-4">Checkout</h2>
+            <form onSubmit={handleSubmit(onSubmit)}>
+              {/* Shipping Information */}
+              <div className="mb-4">
+                <h3 className="text-xl font-semibold mb-2">Shipping Information</h3>
+                <div className="grid grid-cols-2 gap-4">
+                  <div>
+                    <label htmlFor="firstName" className="block text-sm font-medium text-gray-700">
+                      First Name
+                    </label>
+                    <input
+                      type="text"
+                      id="firstName"
+                      name="firstName"
+                      defaultValue={firstName?firstName:""}
+                      className="mt-1 p-2 w-full border rounded focus:outline-none focus:border-blue-500"
+                      {...register("firstName", { required: true })}
+                    />
+                  </div>
+                  <div>
+                    <label htmlFor="lastName" className="block text-sm font-medium text-gray-700">
+                      Last Name
+                    </label>
+                    <input
+                      type="text"
+                      id="lastName"
+                      name="lastName"
+                      defaultValue={lastName?lastName:""}
+                      className="mt-1 p-2 w-full border rounded focus:outline-none focus:border-blue-500"
+                      {...register("lastName", { required: true })}
+                    />
+                  </div>
+                </div>
+                <div className="mt-4">
+                  <label htmlFor="address" className="block text-sm font-medium text-gray-700">
+                    Address
+                  </label>
+                  <input
+                    type="text"
+                    id="address"
+                    name="address"
+                    defaultValue={address?address:""}
+                    className="mt-1 p-2 w-full border rounded focus:outline-none focus:border-blue-500"
+                    {...register("address", { required: true })}
+                  />
+                </div>
+                {/* Add more shipping fields as needed */}
+              </div>
+
+              {/* Payment Information */}
+              <div className="mb-4">
+                <h3 className="text-xl font-semibold mb-2">Payment Information</h3>
+                <div>
+                  <label htmlFor="cardNumber" className="block text-sm font-medium text-gray-700">
+                    Currency
+                  </label>
+                  <select {...register("currency", { required: true })}
+                    className="select select-bordered mt-1 p-2 w-full border rounded focus:outline-none focus:border-blue-500">
+                    <option value="BDT">BDT</option>
+                    <option value="USD">USD</option>
+                    <option value="RMB">RMB</option>
+                    <option value="EURO">EURO</option>
+                  </select>
+                </div>
+                <div className="mt-4">
+                  <label htmlFor="expiryDate" className="block text-sm font-medium text-gray-700">
+                    Mobile
+                  </label>
+                  <input
+                    type="text"
+                    id="mobile"
+                    name="mobile"
+                    defaultValue={phone?phone:""}
+                    className="mt-1 p-2 w-full border rounded focus:outline-none focus:border-blue-500"
+                    {...register("mobile", { required: true })}
+                  />
+                </div>
+                <div className="mt-4">
+                  <label htmlFor="cvv" className="block text-sm font-medium text-gray-700">
+                    Amount
+                  </label>
+                  <input
+                    type="text"
+                    id="amount"
+                    name="amount"
+                    value={data.state && parseInt(data.state.total).toFixed(2)}
+                    className="mt-1 p-2 w-full border rounded focus:outline-none focus:border-blue-500"
+                    {...register("amount", { required: true })}
+                  />
+                </div>
+              </div>
+
+              {/* Place Order Button */}
+              <div className="mt-6">
+                <input
+                  type="submit"
+                  value='Place Order'
+                  className="w-full bg-blue-500 text-white p-2 rounded focus:outline-none hover:bg-blue-700"
+                />
+              </div>
+            </form>
           </div>
         </div>
       </div>
